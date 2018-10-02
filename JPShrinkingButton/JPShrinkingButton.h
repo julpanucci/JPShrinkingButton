b33cbac63a2f6e7712acf365527a3c94116f3035
Julian Panucci <panuccij2@upmc.edu>
2018-10-02 17:07:55 -0400

    Initial Commit

---
+++ JPShrinkingButton/JPShrinkingButton.h
@@ -0,0 +1,19 @@
+//
+//  JPShrinkingButton.h
+//  JPShrinkingButton
+//
+//  Created by Panucci, Julian on 10/2/18.
+//  Copyright © 2018 Panucci, Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JPShrinkingButton.
+FOUNDATION_EXPORT double JPShrinkingButtonVersionNumber;
+
+//! Project version string for JPShrinkingButton.
+FOUNDATION_EXPORT const unsigned char JPShrinkingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JPShrinkingButton/PublicHeader.h>
+
+
